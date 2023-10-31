153bbb66064c7e9d4684ad17056048f32038f90a
daren-scerri <daren.scerri@mcast.edu.mt>
2023-10-31 09:04:22 +0100

    Added cannonball inheritance from DefaultBullet

--- Inheritance_Compositon_Lecture_DGD61A/Assets/Scripts/DefaultBullet.cs
+++ Inheritance_Compositon_Lecture_DGD61A/Assets/Scripts/DefaultBullet.cs
@@ -6,17 +6,37 @@ using UnityEngine;
 
 public class DefaultBullet : MonoBehaviour
 {
-    [SerializeField] protected float yspeed;
-    Rigidbody2D rb;
+    [SerializeField] protected float yspeed, xspeed;
+    protected Rigidbody2D rb;
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
-        yspeed = 300f;
+    }
+    protected virtual void Start()
+    {
+       
+        
+        
         Vector2 myforce = new Vector2(0, yspeed);
         rb.AddForce(myforce);
     }
+
+    /* Add a new method to :
+ * 
+ * 1. apply a force on the horizontal axis
+ * 2. set the gravity to 0.98
+ */
+    protected void HorizontalProjectile()
+    {
+        rb.gravityScale = 0.98f;
+        Vector2 myforce = new Vector2(xspeed, 0f);
+        rb.AddForce(myforce);
+    }
+
+
 
 
 
